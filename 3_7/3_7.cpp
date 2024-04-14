ec0e875216c2eed10c8c2820ac558037a20d4b6a
pppato <108100239+pppato@users.noreply.github.com>
2024-04-14 13:58:33 -0300

    Add files via upload

---
+++ 3_7/3_7.cpp
@@ -0,0 +1,34 @@
+/*7. Realice una función que dado un vector dinámico de enteros, remueva una cantidad de
+elementos parados por parámetros. Dicha función debe comenzar a eliminar elementos
+desde el final.*/
+
+#include <iostream>
+using namespace std;
+
+int *remueve (int *old, int tam, int cant){
+ int *newvec=new int [tam-cant];
+ for(int i=0;i<tam-cant;i++){
+	 newvec[i]=old[i];
+ }
+ delete[]old;
+ return newvec;
+}
+int main(int argc, char *argv[]) {
+	int tam=5, elem=3;
+	int *vec = new int [tam];
+	
+	for (int i=0;i<tam;i++){
+		vec[i]=i;
+		cout<<vec[i]<<" ";
+	}
+	cout<<endl;
+	int *nuevovec=remueve(vec,tam,elem);
+	for (int i=0;i<tam-elem;i++){
+		cout<<nuevovec[i]<<" ";
+	}
+	
+	delete[]nuevovec;
+	
+	return 0;
+}
+
---
+++ 3_8/3_8.cpp
@@ -0,0 +1,38 @@
+/*8. Realice una función que dado un vector dinámico de enteros, retorne un vector con el doble
+de elementos y los nuevos elementos deben ser una copia de los anteriores.
+*/
+#include <iostream>
+using namespace std;
+int *DobleElem(int *old, int tam){
+	
+	int *newvec=new int [tam*2];
+	for (int i=0;i<tam;i++){
+		newvec[i]=old[i];
+		for (int i=tam;i<tam*2;i++){
+			newvec[i]=newvec[i-tam];
+			
+		}
+	}
+	delete[]old;
+	return newvec;
+}
+int main(int argc, char *argv[]) {
+	int cant=5;
+	int * vec= new int [cant];
+	
+	for (int i=0;i<cant;i++){
+		vec[i]=i;
+		cout<<vec[i]<<" ";
+	}
+	cout<<endl;
+	
+	int *newvec=DobleElem(vec,cant);
+	
+	for (int i=0;i<cant*2;i++){
+		cout<<newvec[i]<<" ";
+	}
+	
+	delete[]vec;
+	return 0;
+}
+
